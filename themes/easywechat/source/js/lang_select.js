408071d2c926326f791f80b9dd669a1a3b3718d9
overtrue <anzhengchao@gmail.com>
2016-01-02 21:41:44 +0800

    CSS.

--- themes/easywechat/source/js/lang_select.js
+++ themes/easywechat/source/js/lang_select.js
@@ -4,7 +4,7 @@
   function changeLang(){
     var lang = this.value;
     if (lang === 'zh-cn') lang = '';
-    location.href = window.location.href.replace(/(zh-cn|en)/g, lang);;
+    location.href = window.location.href.replace(/(zh-cn|en)/g, lang);
   }
 
   document.getElementById('lang-select').addEventListener('change', changeLang);
